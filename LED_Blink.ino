8d624509fb3a87fa916ca612657a062b9ccbc749
ashishkumaryadav64305-byte <ashishkumaryadav64305@gmail.com>
2026-09-22 10:36:31 +0530

    Add Arduino LED Blink example code

---
+++ LED_Blink.ino
@@ -0,0 +1,18 @@
+// Arduino LED Blink
+// Author: Ashish Yadav
+
+const int ledPin = 13;
+
+void setup()
+{
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(ledPin, HIGH); // LED ON
+  delay(1000);                // Wait 1 second
+
+  digitalWrite(ledPin, LOW);  // LED OFF
+  delay(1000);                // Wait 1 second
+}
